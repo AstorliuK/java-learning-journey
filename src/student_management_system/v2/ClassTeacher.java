42381822ea6200d5dee0a49d0927557b3dd263dc
AstorliuK <Astorliuk@gmail.com>
2025-10-14 23:55:13 +0800

    Add files via upload

---
+++ src/student_management_system/v2/ClassTeacher.java
@@ -0,0 +1,41 @@
+package student_management_system.v2;
+
+public class ClassTeacher {
+    private String teacherName; //定义老师的名字
+    private int age; //定义老师的年龄
+    private String sex; //定义老师的性别
+
+    public ClassTeacher(String teacherName,int age,String sex) {
+        this.teacherName = teacherName;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public String getTeacherName() {
+        return teacherName;
+    }
+
+    public void setTeacherName(String teacherName) {
+        this.teacherName = teacherName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String showInfo() {
+        return "班主任名字："+teacherName+"年龄："+age+"性别："+sex;
+    }
+}
---
+++ src/student_management_system/v2/Main.java
@@ -0,0 +1,9 @@
+package student_management_system.v2;
+
+public class Main {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.tini();
+
+    }
+}
