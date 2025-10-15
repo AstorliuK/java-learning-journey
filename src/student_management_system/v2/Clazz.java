00abd65036ea8991eb730228df8b96a354694eaf
AstorliuK <Astorliuk@gmail.com>
2025-10-15 23:24:37 +0800

    update student_management_system_v2

---
+++ src/student_management_system/v2/Clazz.java
@@ -0,0 +1,179 @@
+package student_management_system.v2;
+
+public class Clazz {
+    private String major; //定义班级的专业
+    private String grade; //定义班级的年级
+    private Student[] students = null; //定义一个只包含Student对象的数组为空值
+    private int studentIndex = 0; //定义一个添加的是第几个学生的索引
+
+    //定义一个初始化班级信息的构造方法,包括专业、年级和学生信息
+    public Clazz(String major, String grade) {
+        this.major = major;
+        this.grade = grade;
+        students = new Student[50]; //50长度对应50个学生
+        students[0] = new Student("001","小明",23,"男");
+        students[1] = new Student("002","红红",23,"女");
+        students[2] = new Student("003","小刚",23,"男");
+        students[3] = new Student("004","小张",23,"男");
+        students[4] = new Student("005","李李",23,"女");
+        studentIndex = 5;
+        /*
+         *
+         */
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public Student[] getStudents() {
+        return students;
+    }
+
+    public void setStudents(Student[] students) {
+        this.students = students;
+    }
+
+    //统计当前学生的人数
+    private int countStudents() {
+        int count = 0;
+        int i = 0;
+        while (i < students.length && students[i] != null) {
+            count++;
+            i++;
+        }
+        return count;
+
+        /*也可以用for循环来实现
+         * int count = 0;
+         * for(int i = 0; i<students.length;i++){
+         *     if(student[i] != null){
+         *      count++
+         *     }
+         * }
+         * return count;
+         *
+         * for循环更加的实用和简便。
+         * 我一开始用的while循环忘了考虑数据溢出了，没有限制i，后面用短路与补救。
+         * 而for循环则可以从一开始就考虑到。
+         */
+    }
+
+    //判断学生是否满了，一个班级里最多存在50个学生。
+    public boolean isFull() {
+        return studentIndex >= 50;
+        /* 一开始用的三元判断，大于等于50人判定满了返回true，否则返回false。
+         * 也考虑用普通的if else循环，不过都没必要，直接写就可以返回布尔式的结果的。
+         */
+    }
+
+    //添加学生的方法，首先运用了已经创建的isStudentExist方法，确定学生有无创建
+    public void addStudent(Student student) {
+        if(isFull()) {
+            System.out.println("班级已满，无法添加学生！");
+            return;
+        }
+        //检查时更改的bug，在添加时忘了使用isFull检查数组是否已满了
+        if (isStudentExist(student.getId())) {
+            System.out.println("添加的学号已经存在！");
+        } else {
+            students[studentIndex] = student;
+            studentIndex++;
+        }
+    }
+
+    //删除学生的方法,通过学号删除
+    public void deleteStudent(String deleteId) {
+        if (isStudentExist(deleteId)) {
+            for (int i = 0; i < studentIndex; i++) {
+                if (students[i].getId().equals(deleteId)) {
+                    for (int j = i; j < studentIndex - 1; j++) {
+                        //检查时更改的bug，要-1，防止下面的数组越界
+                        students[j] = students[j + 1];
+                    }
+                    students[studentIndex] = null;
+                    studentIndex--;
+                }
+            }
+        } else {
+            System.out.println("你所输入的学号不存在！");
+        }
+    }
+
+    //修改学生的方法
+    public void updateStudent(Student student) {
+        if (isStudentExist(student.getId())) {
+            for (int i = 0; i < studentIndex; i++) {
+                if (students[i].getId().equals(student.getId())) {
+                    students[i] = student;
+                }
+            }
+        } else {
+            System.out.println("你所输入的学号不存在！");
+        }
+    }
+
+    //判断学生是否存在的方法,通过学号来进行判断，用equals方法
+    public boolean isStudentExist(String studentId) {
+        boolean result = false;
+        for (int i = 0; i < studentIndex; i++) {
+            if (students[i].getId().equals(studentId)) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+        /* 要注意在循环体中，不能够直接的进行return，否则会报错。
+         * 要先定义一个boolean类型的变量，并赋一个默认的值。
+         * 在循环体达成任务后，再在循环体中把这个变量重新赋值。
+         * 最后在循环体外return。
+         */
+    }
+
+    //根据学号查找学生
+    public Student selectStudentById(String studentId) {
+        Student result = null;
+        for (int i = 0; i < studentIndex; i++) {
+            if (students[i].getId().equals(studentId)) {
+                result = students[i];
+                break;
+            }
+        }
+        if (result == null) {
+            System.out.println("你所输入的学号不存在！");
+        }
+        return result;
+    }
+
+    //打印班级所有学生的信息
+    public void printClazzInfo() {
+        System.out.println("班级信息如下：");
+        System.out.println("**********************");
+        System.out.println(showInfo());
+        System.out.println(Menu.classTeacher.showInfo());
+        for (int i = 0; i < studentIndex; i++) {
+            System.out.println(students[i].showInfo());
+       }
+        System.out.println("**********************");
+    }
+
+    //打印班级信息
+    public String showInfo(){
+        return grade+"-"+major+"专业"+"-学生人数："+countStudents();
+    }
+
+
+}
+
---
+++ src/student_management_system/v2/Menu.java
@@ -0,0 +1,138 @@
+package student_management_system.v2;
+
+import com.sun.source.tree.NewArrayTree;
+
+import java.util.Scanner;
+
+
+    //内部调用
+
+public class Menu {
+    //班主任是班级唯一的，50个学生共用1个班主任，所以可以设置成static
+    public static ClassTeacher classTeacher = new ClassTeacher("张老师",33,"男");
+
+    //创建班级对象,假设管理的班级也是唯一固定的
+    public static Clazz clazz = new Clazz("软件工程","2025级");
+
+    //学生的总人数,暂时可能没啥用
+    public int studentNumber = 0;
+
+    //创建输入流
+    public Scanner sc = new Scanner(System.in);
+
+    //创建一个Menu的构造方法
+    public Menu(){
+
+    /* 可以Clazz的构造方法中的创建学生信息移到这里
+     * 符合单一职责原则，让代码更加简易可读、易于维护
+     */
+    }
+
+
+    //创建菜单头部
+    private void printHeader() {
+        System.out.println("==================");
+        System.out.println("欢迎使用班级管理系统！");
+        System.out.println("==================");
+    }
+
+    //创造用于菜单的数组
+    String[] useMenu = new String[]{"1.显示学生信息", "2.添加学生信息", "3.删除学生信息"
+            , "4.修改学生信息", "5.根据学号查询", "6.退出系统"};
+
+    //创造菜单
+    private void printMenu() {
+        for (String i : useMenu) {  //遍历菜单数组
+            System.out.println(i);
+        }
+        System.out.println("==================");
+        System.out.println("请输入序号使用功能：");
+        }
+
+    //初始化的方法，进行程序的初始化
+    private void initSystem(){
+        printHeader();
+        printMenu();
+    }
+
+
+    //执行菜单的方法
+    public void startSystem(){
+        initSystem(); //这里调用初始化的方法
+        int num = 0;
+        while (num != 6) {
+            num = sc.nextInt();
+            switch (num) {
+            case 1:
+                clazz.printClazzInfo();
+                break;
+            case 2:
+                System.out.println("请输入学生的学号：");
+                String id = sc.next();
+                System.out.println("请输入学生的姓名：");
+                String name = sc.next();
+                System.out.println("请输入学生的年龄：");
+                int age = sc.nextInt();
+                System.out.println("请输入学生的性别：");
+                String sex = sc.next();
+                Student student = new Student(id,name,age,sex);
+                clazz.addStudent(student);
+                clazz.printClazzInfo();
+                break;
+            case 3:
+                System.out.println("请输入需要删除的学生学号：");
+                String deleteId = sc.next();
+                clazz.deleteStudent(deleteId);
+                clazz.printClazzInfo();
+                break;
+            case 4:
+                System.out.println("请输入学生的学号：");
+                String updateId = sc.next();
+                System.out.println("请输入学生的姓名：");
+                String updateName = sc.next();
+                System.out.println("请输入学生的年龄：");
+                int updateAge = sc.nextInt();
+                System.out.println("请输入学生的性别：");
+                String updateSex = sc.next();
+                Student updateStudent = new Student(updateId,updateName,updateAge,updateSex);
+                clazz.updateStudent(updateStudent);
+                clazz.printClazzInfo();
+                break;
+            case 5:
+                System.out.println("请输入学生的学号：");
+                String selectId = sc.next();
+                Student select = clazz.selectStudentById(selectId);
+                /* 这行代码其实就是命名一个Student类型的select变量。
+                 * 然后将clazz.selectStudentById(selectId)，这行内容的结果指向它。
+                 * 这并不是创建新对象……就单纯的赋值，不过这行代码是引用，是内存地址的指向。
+                 */
+                if(select!=null){
+                    System.out.println("查询成功！你所查询的学生信息如下：");
+                    System.out.println(select.showInfo());
+                }
+                /* 大晚上的，忽然有点发蒙，要先创建对象才能使用里面的方法啊。
+                 * 但这里，虽然select没有new，但是他指向了静态对象clazz啊，即被赋予了内存地址了。
+                 * clazz的构造方法中，是有new Student的，所以他也能用showInfo()。
+                 */
+                break;
+            case 6:
+                System.out.println("选择功能：退出系统");
+                System.out.println("=======================");
+                System.out.println("系统退出成功，欢迎下次使用！");
+                System.out.println("=======================");
+                exitSystem();
+                break;
+            default:
+                System.out.println("输入无效，请重新输入1-6之间的数字");
+                break;
+            }
+        if(num != 6){
+            initSystem();
+        }
+        }
+    }
+    //关闭程序的方法
+    public void exitSystem(){
+        sc.close(); //关掉Scanner输入流
+    }
+}
